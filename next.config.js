38a889524994c8d606c054362750b3977c585371
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-27 10:09:13 -0300

    ts config specifc setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const { withFrameworkConfig } = require("./framework/common/config");
+
 module.exports = {
-  reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US"
+  }
+};
