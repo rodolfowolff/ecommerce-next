500ad01950171173da24d1c39b2c23cfe5e367b1
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-27 10:22:50 -0300

    format json in config file

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 const fs = require("fs");
 const merge = require("deepmerge");
+const prettier = require("prettier");
 
 function withFrameworkConfig(defaultConfig = {}) {
-  const framework = "shopify";
+  const framework = defaultConfig?.framework.name;
 
   const frameworkNextConfig = require(path.join("../", framework, "next.config"));
   const config = merge(defaultConfig, frameworkNextConfig);
@@ -16,8 +17,8 @@ function withFrameworkConfig(defaultConfig = {}) {
 
   fs.writeFileSync(
     tsPath,
-    JSON.stringify(
-      tsConfig, null, 2
+    prettier.format(
+      JSON.stringify(tsConfig), { parser: "json" }
     )
   );
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const { withFrameworkConfig } = require("./framework/common/config");
 
 module.exports = {
+  framework: {
+    name: "shopify"
+  },
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US"
