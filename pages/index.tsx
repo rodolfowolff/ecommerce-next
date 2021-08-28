9e78235edeab5251f265892fa2de1e8905a3a957
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-28 01:07:48 -0300

    add grid template

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import getAllProducts from '@framework/product/get-all-product';
 import { getConfig } from '@framework/api/config';
 import { Layout } from '@components/common';
 import { ProductCard } from '@components/common/product/ProductCard';
+import { Grid } from '@components/ui';
 
 export async function getStaticProps() {
   const config = getConfig();
@@ -20,13 +21,15 @@ export default function Home({
   products
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className="root">
-      {products.slice(0, 3).map(product =>
-        <ProductCard
-          key={product.id}
-          product={product}
-        />
-      )}
-    </div>
+    <>
+      <Grid>
+        {products.slice(0, 3).map(product =>
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        )}
+      </Grid>
+    </>
   )
 }
