7bd361a59d898f7e4008ac7371588b521eb80089
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-29 00:43:47 -0300

    add footer and navbar

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { FC } from "react";
 import s from "./Layout.module.css";
+import { Footer, Navbar } from "@components/common";
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={s.root}>
-      {children}
+      <Navbar />
+      <main className="fit">
+        {children}
+      </main>
+      <Footer />
     </div>
   )
 }
